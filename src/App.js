4c01c27254908638b227546f659ea9af18a0c6c0
SarkerPabon <sarkerpabon@gmail.com>
2022-05-07 20:06:31 +0600

    Fix Navigation and make responsive

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
 					}
 				/>
 				<Route
-					path='/products/myProducts'
+					path='/myProducts'
 					element={
 						<RequiredAuth>
 							<MyProducts />
--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -7,31 +7,58 @@ import auth from "../firebase.init";
 const Header = () => {
 	const [user] = useAuthState(auth);
 
+	const navLinkStyles = ({ isActive }) => {
+		return {
+			borderBottom: isActive ? "1px solid white" : "",
+			fontWeight: isActive ? "bold" : "normal",
+		};
+	};
+
 	return (
-		<>
-			<Navbar bg='primary' variant='dark'>
-				<Container>
-					<Navbar.Brand as={Link} to='/'>
-						Car Warehouse
-					</Navbar.Brand>
-					<Nav className='ms-auto'>
-						<Nav.Link as={NavLink} to='/' className='text-white'>
+		<Navbar bg='secondary' expand='lg'>
+			<Container>
+				<Navbar.Brand as={Link} to='/' className='text-white'>
+					Navbar scroll
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls='navbarScroll' />
+				<Navbar.Collapse id='navbarScroll'>
+					<Nav
+						className='ms-auto my-2 my-lg-0'
+						style={{ maxHeight: "100px" }}
+						navbarScroll
+					>
+						<Nav.Link
+							as={NavLink}
+							to='/'
+							className='text-white'
+							style={navLinkStyles}
+						>
 							Home
 						</Nav.Link>
-
 						{user ? (
 							<>
-								<Nav.Link as={NavLink} to='/products' className='text-white'>
+								<Nav.Link
+									as={NavLink}
+									to='/products'
+									className='text-white'
+									style={navLinkStyles}
+								>
 									Manage Products
 								</Nav.Link>
 								<Nav.Link
 									as={NavLink}
-									to='/products/myProducts'
+									to='/myProducts'
 									className='text-white'
+									style={navLinkStyles}
 								>
 									{user.displayName ? user.displayName + " " : "My "} Products
 								</Nav.Link>
-								<Nav.Link as={NavLink} to='/addProduct' className='text-white'>
+								<Nav.Link
+									as={NavLink}
+									to='/addProduct'
+									className='text-white'
+									style={navLinkStyles}
+								>
 									Add Product
 								</Nav.Link>
 								<button
@@ -43,18 +70,28 @@ const Header = () => {
 							</>
 						) : (
 							<>
-								<Nav.Link as={NavLink} to='/register' className='text-white'>
+								<Nav.Link
+									as={NavLink}
+									to='/register'
+									className='text-white'
+									style={navLinkStyles}
+								>
 									Register
 								</Nav.Link>
-								<Nav.Link as={NavLink} to='/login' className='text-white'>
+								<Nav.Link
+									as={NavLink}
+									to='/login'
+									className='text-white'
+									style={navLinkStyles}
+								>
 									Login
 								</Nav.Link>
 							</>
 						)}
 					</Nav>
-				</Container>
-			</Navbar>
-		</>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
 	);
 };
 
