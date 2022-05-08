a864fe738fc4bca56e2dde0f8dc04c972c7c55d5
SarkerPabon <sarkerpabon@gmail.com>
2022-05-08 11:04:58 +0600

    Add functionality for QTY

--- src/Pages/Products/ProductDetails.js
+++ src/Pages/Products/ProductDetails.js
@@ -7,6 +7,7 @@ const ProductDetails = () => {
 	const [product, setProduct] = useState({});
 	const [loading, setLoading] = useState(true);
 	const [isRefresh, setIsRefresh] = useState(false);
+	const [addQty, setAddQty] = useState(0);
 	const { id } = useParams();
 
 	const { _id, name, imgUrl, description, price, quantity, sold } = product;
@@ -28,6 +29,26 @@ const ProductDetails = () => {
 			.catch((error) => console.log(error));
 	};
 
+	const handleQtyUpdate = (e) => {
+		e.preventDefault();
+		console.log({ addQty: parseInt(addQty) });
+
+		if (parseInt(addQty) !== 0) {
+			axios
+				.patch(
+					`https://wearhouse-management-mern.herokuapp.com/products/${id}/qty`,
+					{
+						addQty: parseInt(addQty),
+					}
+				)
+				.then((response) => {
+					setIsRefresh(!isRefresh);
+					setAddQty(0);
+				})
+				.catch((error) => console.log(error));
+		}
+	};
+
 	return (
 		<div className='container my-5'>
 			<div className='row justify-content-center'>
@@ -35,27 +56,37 @@ const ProductDetails = () => {
 				{loading ? (
 					<Loader />
 				) : (
-					<div className='col-md-6 col-sm-12'>
+					<div className='col-md-8 col-sm-12'>
 						<div className='card'>
 							<img src={imgUrl} className='card-img-top' alt='' />
 							<div className='card-body'>
 								<h5 className='card-title text-center mb-2'>{name}</h5>
 								<p className='card-text'>{description}</p>
-								<form
-									onSubmit={handleDeliver}
-									className='d-flex justify-content-between mb-2'
-								>
+								<div className='d-flex justify-content-between mb-2'>
 									<h5 className='card-subtitle'>Price: {price}</h5>
 									<h5 className='card-subtitle'>Qty: {quantity}</h5>
 									<h5 className='card-subtitle'>Sold: {sold}</h5>
-								</form>
-								<div className='d-flex justify-content-between'>
+								</div>
+								<div className='d-flex justify-content-between  mb-2 mt-4'>
 									<button
 										onClick={handleDeliver}
-										className='btn btn-outline-secondary text-decoration-none'
+										className='btn btn-outline-secondary text-decoration-none '
 									>
 										Delivered
 									</button>
+									<form onSubmit={handleQtyUpdate} className=''>
+										<input
+											type='text'
+											name='addQty'
+											id='addQty'
+											className='p-1 '
+											value={addQty}
+											onChange={(e) => setAddQty(e.target.value)}
+										/>
+										<button className='btn btn-outline-secondary ms-1 mb-1 p-1'>
+											Add QTY
+										</button>
+									</form>
 								</div>
 							</div>
 						</div>
